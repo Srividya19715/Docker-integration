236f1a2e47d465408d2bc01d2cdf12ef60649ddf
Srividya19715 <manishahemadri03@gmail.com>
2021-11-20 10:03:06 +0530

    First commit

---
+++ src/main/java/scneario4/MavenSample1/App.java
@@ -0,0 +1,13 @@
+package scneario4.MavenSample1;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
